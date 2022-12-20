eefa701047e3f8e45e2d425de433120b5150d379
Jose Artavia <jartavia05@gmail.com>
2022-12-20 17:25:05 -0600

    basi docker bash

--- basic_docker.sh
+++ basic_docker.sh
@@ -17,3 +17,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 echo "Docker installation successfully." 
+echo "Starting the instalation of docker-compose"
+
+curl -SL https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
