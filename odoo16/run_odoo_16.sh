1c990726147b4d2f6415f7a339d6f074583b70d3
Jose Artavia <jartavia05@gmail.com>
2023-01-24 17:44:12 -0600

    updating the path of addons in the script

--- odoo16/run_odoo_16.sh
+++ odoo16/run_odoo_16.sh
@@ -1,1 +1,1 @@
-docker run -v /opt/uat_odoo_infra/odoo16/addons/condo_admin_odoo:/mnt/extra-addons -p 9010:8069 --name odoo16 --link db:db -t -d odoo:16.0
+docker run -v /opt/uat_odoo_infra/odoo16/addons/:/mnt/extra-addons -p 9010:8069 --name odoo16 --link db:db -t -d odoo:16.0
